78660c1d5292790b8559e960065c0aff50ec643d
Tokollo NONYANE <ST10296818@rcconnect.edu.za>
2024-04-07 16:22:37 +0200

    Added a new class

---
+++ Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10296818_PROG6221_Tokollo_Will_Nonyane_POE_Part_1
+{
+    internal class Methods
+    {
+        public static void close()
+        {
+            Console.WriteLine("Thank you for using the system");
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
