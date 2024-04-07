d331667792a60802d330cbe62f42b09e14fd148e
Tokollo NONYANE <ST10296818@rcconnect.edu.za>
2024-04-07 16:37:55 +0200

    Added while loop for the menu

--- Program.cs
+++ Program.cs
@@ -7,7 +7,39 @@ namespace POE
     {
         static void Main(string[] args)
         {
-            Methods.Start();
+            int option;
+            do
+            {
+                Methods.Start();
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        //Method used to add ingredients
+                        Methods.add();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+                        End.close();
+                        break;
+                    default:
+                        Console.WriteLine("Invaild option. Please pick a option shown above");
+                        break;
+                }
+            }
+            while (option != 6);
+            
         }
     }
 }
